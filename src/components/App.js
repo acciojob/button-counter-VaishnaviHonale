49fd756dc63f6163b0e2a75d4ea9b52e6e8361a0
Vaishnavi <vaishnavihonale02@gmail.com>
2023-11-30 22:21:09 +0530

    deploy

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,17 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count,setCount]=useState(0);
+  const handleButtonClick=()=>{
+    setCount(count+1);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <button onClick={handleButtonClick}>Click me</button>
+        <p>Button Clicked{count}times</p>
     </div>
   )
 }
 
-export default App
+export default App;
